8e45a525c73a3a4f6fe8d342f38d6afa0e1c5f4b
Alina Gotcherian <alina.gotcherian@gmail.com>
2022-07-01 09:27:54 -0400

    basic setup for header

---
+++ picsnap.root/header.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include_once "includes/config/db_config.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- HEAD -->
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Homing Pigeon</title>
+  <meta name="description" content="Discover postcard designs from Montreal artists">
+  <meta name="author" content="Ali Nehme, Alina Gotcherian Edgar Townsend, Mitchell Fridman, Rami Chaouki">
+  <!--
+      Web Development Final Team Project
+      Developed by: Ali Nehme, Alina Gotcherian Edgar Townsend, Mitchell Fridman, Rami Chaouki
+      Last Update: June 30, 2022
+      -->
+
+  <link rel="icon" type="image/png" href="images/homing_pigeon_logo.png" />
+
+  <!-- LINK TO BOOTSTRAP CSS LIBRARY -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <!-- LINK TO BOOTSTRAP ICONS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+  <!-- LINK TO LOCAL STYLESHEET -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- LINK TO JQUERY -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+</head>
+
+<body>
+  <p>Hello world</p>
+</body>
+
+</html>
--- picsnap.root/includes/config/db_config.php
+++ picsnap.root/includes/config/db_config.php
@@ -3,11 +3,11 @@
 $servername = 'localhost';
 $username = 'root';
 $serpass = '';
-$databasename = '';
+$databasename = 'homing_pigeon';
 
 
-$conn = mysqli_connect($servername,$username,$serpass ,$databasename );
+$conn = mysqli_connect($servername, $username, $serpass, $databasename);
 
-if(!$conn){
-    die("Connection failed: ".mysqli_connect_error());
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
