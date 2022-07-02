cf10afc73e4d93c06b7b1557bfec5f52e5209163
Alina Gotcherian <alina.gotcherian@gmail.com>
2022-07-01 21:02:11 -0400

    Header first draft complete. I added the header requirement on each page.

--- picsnap.root/includes/functions.inc.php
+++ picsnap.root/includes/functions.inc.php
@@ -7,17 +7,17 @@
  */
 function check_if_favourite($postcard_id)
 {
-    require "config/db_config.php";
-    $userid = $_SESSION['id'];
-    $sql = "SELECT * FROM favourites where postcard_id='$postcard_id' and user_id='$userid' ";
+  require "config/db_config.php";
+  $userid = $_SESSION['id'];
+  $sql = "SELECT * FROM favourites where postcard_id='$postcard_id' and user_id='$userid' ";
 
-    $result = mysqli_query($conn, $sql);
-    $numofrows = mysqli_num_rows($result);
-    if ($numofrows >= 1) {
-        return true;
-    } else {
-        return false;
-    }
+  $result = mysqli_query($conn, $sql);
+  $numofrows = mysqli_num_rows($result);
+  if ($numofrows >= 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -28,14 +28,14 @@ function check_if_favourite($postcard_id)
 function unpaid_cart_id()
 {
 
-    require "config/db_config.php";
+  require "config/db_config.php";
 
-    $userid = $_SESSION['id'];
+  $userid = $_SESSION['id'];
 
-    $cart_id_array = mysqli_query($conn, "SELECT max(id) as cart_id FROM carts WHERE user_id='$userid' AND is_paid='0'");
-    $cart_id = mysqli_fetch_array($cart_id_array)["cart_id"];
+  $cart_id_array = mysqli_query($conn, "SELECT max(id) as cart_id FROM carts WHERE user_id='$userid' AND is_paid='0'");
+  $cart_id = mysqli_fetch_array($cart_id_array)["cart_id"];
 
-    return $cart_id;
+  return $cart_id;
 }
 
 
@@ -46,23 +46,23 @@ function unpaid_cart_id()
  */
 function check_if_in_cart($postcard_id)
 {
-    require "config/db_config.php";
+  require "config/db_config.php";
 
-    $cart_id = unpaid_cart_id();
+  $cart_id = unpaid_cart_id();
 
-    $sql = "SELECT *
+  $sql = "SELECT *
             FROM carts_postcards
             WHERE cart_id = '$cart_id'
             AND postcard_id = '$postcard_id'";
 
-    $result = mysqli_query($conn, $sql);
-    $numofrows = mysqli_num_rows($result);
+  $result = mysqli_query($conn, $sql);
+  $numofrows = mysqli_num_rows($result);
 
-    if ($numofrows >= 1) {
-        return true;
-    } else {
-        return false;
-    }
+  if ($numofrows >= 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -72,11 +72,11 @@ function check_if_in_cart($postcard_id)
  */
 function fetch_db_table($table_name)
 {
-    require "config/db_config.php";
-    $sql = "SELECT * FROM $table_name";
+  require "config/db_config.php";
+  $sql = "SELECT * FROM $table_name";
 
-    $dbresult = mysqli_query($conn, $sql);
-    return $dbresult;
+  $dbresult = mysqli_query($conn, $sql);
+  return $dbresult;
 }
 
 /**
@@ -86,12 +86,12 @@ function fetch_db_table($table_name)
  */
 function fetch_db_table_by_id($table_name, $id)
 {
-    require "config/db_config.php";
-    $sql = "SELECT * FROM $table_name where id='$id'";
+  require "config/db_config.php";
+  $sql = "SELECT * FROM $table_name where id='$id'";
 
-    $dbresult = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($dbresult);
-    return $row;
+  $dbresult = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($dbresult);
+  return $row;
 }
 
 /**
@@ -101,28 +101,28 @@ function fetch_db_table_by_id($table_name, $id)
  */
 function filter_postcards()
 {
-    require "config/db_config.php";
+  require "config/db_config.php";
+  $dbresult = fetch_db_table("postcards");
+
+  if (!empty($_POST["name"])) {
+    $name = $_POST['name'];
+    $sql = "SELECT * FROM postcards WHERE name LIKE '%$name%'";
+
+    $dbresult = mysqli_query($conn, $sql);
+  }
+
+  if (!empty($_POST['artist'])) {
+    $artist = $_POST['artist'];
+    $sql = "SELECT * FROM postcards WHERE artist LIKE '%$artist%'";
+
+    $dbresult = mysqli_query($conn, $sql);
+  }
+
+  if (!empty($_POST["reset"])) {
     $dbresult = fetch_db_table("postcards");
+  }
 
-        if (!empty($_POST["name"])) {
-            $name = $_POST['name'];
-            $sql = "SELECT * FROM postcards WHERE name LIKE '%$name%'";
-
-            $dbresult = mysqli_query($conn, $sql);
-        }
-
-        if (!empty($_POST['artist'])) {
-            $artist = $_POST['artist'];
-            $sql = "SELECT * FROM postcards WHERE artist LIKE '%$artist%'";
-
-            $dbresult = mysqli_query($conn, $sql);
-        }
-    
-        if (!empty($_POST["reset"])) {
-            $dbresult = fetch_db_table("postcards");
-        }
-
-    return $dbresult;
+  return $dbresult;
 }
 
 /**
@@ -130,14 +130,14 @@ function filter_postcards()
  * A function that adds a postcard to favourites or cart
  * parameters: none
  */
-function add_postcard() {
+function add_postcard()
+{
 
-    if(array_key_exists('add_to_favourites', $_POST)) {
-       add_to_favourites($_POST['add_to_favourites']);
-    }
-    else if(array_key_exists('add_to_cart', $_POST)) {
-        add_to_cart($_POST['add_to_cart']);
-    }
+  if (array_key_exists('add_to_favourites', $_POST)) {
+    add_to_favourites($_POST['add_to_favourites']);
+  } else if (array_key_exists('add_to_cart', $_POST)) {
+    add_to_cart($_POST['add_to_cart']);
+  }
 }
 
 /**
@@ -145,14 +145,16 @@ function add_postcard() {
  * A function that adds a postcard to favourites
  * parameters: $postcard_id
  */
-function add_to_favourites($postcard_id) {
-    require "config/db_config.php";
+function add_to_favourites($postcard_id)
+{
+  require "config/db_config.php";
 
-        $userid = $_SESSION['id'];
-        
-        $addquery = "INSERT INTO favourites(user_id, postcard_id) values('$userid', '$postcard_id')";
-        if(mysqli_query($conn, $addquery)){header("Refresh:0");}
-    
+  $userid = $_SESSION['id'];
+
+  $addquery = "INSERT INTO favourites(user_id, postcard_id) values('$userid', '$postcard_id')";
+  if (mysqli_query($conn, $addquery)) {
+    header("Refresh:0");
+  }
 }
 
 /**
@@ -160,12 +162,38 @@ function add_to_favourites($postcard_id) {
  * A function that adds a postcard to cart
  * parameters: $postcard_id
  */
-function add_to_cart($postcard_id) {
-    require "config/db_config.php";
+function add_to_cart($postcard_id)
+{
+  require "config/db_config.php";
 
-        $cart_id = unpaid_cart_id();
-        
-        $addquery = "INSERT INTO carts_postcards(cart_id, postcard_id, quantity) values ('$cart_id', '$postcard_id', '1')";
+  $cart_id = unpaid_cart_id();
 
-        if(mysqli_query($conn, $addquery)){header("Refresh:0");}
+  $addquery = "INSERT INTO carts_postcards(cart_id, postcard_id, quantity) values ('$cart_id', '$postcard_id', '1')";
+
+  if (mysqli_query($conn, $addquery)) {
+    header("Refresh:0");
+  }
+}
+
+/**
+ * Alina Gotcherian
+ * A function that checks if a user is an admin
+ * returns a boolean
+ * parameters: none
+ */
+function is_admin()
+{
+  require "config/db_config.php";
+  // Check if there is a user id
+  $userid = $_SESSION['id'];
+  $sql = "SELECT *
+          FROM users
+          WHERE id='$userid' ";
+  $result = mysqli_query($conn, $sql);
+  $numofrows = mysqli_num_rows($result);
+  if ($numofrows >= 1) {
+    if (mysqli_fetch_array($result)['type'] == "admin") return true;
+  } else {
+    return false;
+  }
 }
