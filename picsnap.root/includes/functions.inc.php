0cd42cf9debeb9b1678737f7a1171c153ed8d413
KrakenZim <104575615+KrakenZim@users.noreply.github.com>
2022-07-02 17:10:21 -0400

    functions for sign up and login

--- picsnap.root/includes/functions.inc.php
+++ picsnap.root/includes/functions.inc.php
@@ -64,16 +64,10 @@ function createNewUser($conn,$fname,$lname,$email,$address,$pwd){
         exit();
     }
     $hashedpwd=password_hash($pwd,PASSWORD_DEFAULT);
-    mysqli_stmt_bind_param($stmt,"ss",$fname,$lname,$email,$address,$hashedpwd);
+    mysqli_stmt_bind_param($stmt,"sssss",$fname,$lname,$email,$address,$hashedpwd);
     mysqli_stmt_execute($stmt);
 
-    $resultData=mysqli_stmt_get_result($stmt);
-    if($row=mysqli_fetch_assoc($resultData)){
-        return $row;
-    }else{
-        $result=false;
-        return $result;
-    }
+    
     mysqli_stmt_close($stmt);
     header("location: ../signup.php?error=none");
     exit();
