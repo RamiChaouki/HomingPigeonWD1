3d5328c636be7925780893d46dfc6ffcd7ae696a
mitchellfridman <fridmanmitchell@gmail.com>
2022-07-03 01:39:21 -0400

    Added in a placeholder page for checkout, a cart page, and associated script and stripe functionality

--- picsnap.root/checkout.php
+++ picsnap.root/checkout.php
@@ -4,3 +4,16 @@
 $currentPage = basename(__FILE__, '.php');
 require 'header.php';
 ?>
+
+<h1>Stripe Checkout</h1>
+<button id="btn">Checkout</button>
+<script src="http://js.stripe.com/v3/"></script>
+<script src="script.js"></script> 
+
+
+
+
+
+<?php
+require 'footer.php';
+?>
